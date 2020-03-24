8286d3fc92917e504c912538a92f27dbd77640fa
Zach Rickers <mzrickers@gmail.com>
2020-03-24 15:09:38 -0600

    Header and Footer

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import * as serviceWorker from './serviceWorker';
-import 'typeface-lobster'
-import 'typeface-open-sans'
+import 'typeface-lobster';
+import 'typeface-open-sans';
+import 'font-awesome/css/font-awesome.css';
+import 'bootstrap-social/bootstrap-social.css';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
