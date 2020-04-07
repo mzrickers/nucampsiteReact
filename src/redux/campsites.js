bb8358d849bf39826ba3b5de145653314e149230
Zach Rickers <mzrickers@gmail.com>
2020-04-07 16:55:43 -0600

    Redux Thunk

--- src/redux/campsites.js
+++ src/redux/campsites.js
@@ -1,7 +1,17 @@
-import { CAMPSITES } from '../shared/campsites';
+import * as ActionTypes from './ActionTypes';
 
-export const Campsites = (state = CAMPSITES, action) => {
+export const Campsites = (state = {
+        isLoading: true,
+        errMess: null,
+        campsites: []
+    }, action) => {
     switch(action.type) {
+        case ActionTypes.ADD_CAMPSITES:
+            return {...state, isLoading: false, errMess: false, campsites: action.payload};
+        case ActionTypes.CAMPSITES_LOADING:
+            return {...state, isLoading: true, errMess: false, campsites: []};
+        case ActionTypes.CAMPSITES_FAILED:
+            return {...state, isLoading: false, errMess: true, campsites: action.payload};
         default:
             return state;
     }
